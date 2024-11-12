5036e4f87007b2e14e50e751d065e72a0e3ce079
Cole Cantu <colecantu@Coles-MacBook-Air.local>
2024-11-11 22:32:06 -0500

    fixed ip in pocketbase call

--- Svelte/src/lib/pocketbase.ts
+++ Svelte/src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
 // linode ip goes here, i am not sure about what port tho??
-export const pb = new PocketBase('http://172.105.153.41');
+export const pb = new PocketBase('http://172.105.153.41:8080');
 
 export const records1 = await pb.collection('TotalClicks').getFullList({
     sort: '-created',
